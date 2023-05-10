c2f38cecafef46021dae2d208645acd43069c53b
Damian <Damian@DESKTOP-811K2BF>
2023-05-10 19:15:10 +0100

    added test for sum of columns

--- Lab5UnitTest/Lab5UnitTest.cpp
+++ Lab5UnitTest/Lab5UnitTest.cpp
@@ -9,9 +9,25 @@ namespace Lab5UnitTest
 	TEST_CLASS(Lab5UnitTest)
 	{
 	public:
-		
-		TEST_METHOD(TestMethod1)
+		//Used to test SumOfCols
+
+		TEST_METHOD(TestMethodSumOfCols)
 		{
+			// Arrange
+			const int ROW = 5;
+			double matrix[ROW][MAX_COL] = { {2, 0, 0, 0},
+				{1, 0, 0, 0},
+				{1, 0, 0, 0},
+				{1, 0, 0, 0} };
+			double* ptrRet;
+
+			// Act
+			ptrRet = sumOfCols(matrix, ROW);
+
+			/// Assert
+			Assert::AreEqual(5.0, ptrRet[0]);
+			Assert::AreEqual(0.0, ptrRet[1]);
+			Assert::AreEqual(0.0, ptrRet[2]);
 		}
 	};
 }
