59a7b52f46647abc3cb5ba8fdec3bb0521bf61cf
Damian <Damian@DESKTOP-811K2BF>
2023-05-10 19:06:02 +0100

    added skeleton of unit test project to solution

---
+++ Lab5UnitTest/Lab5UnitTest.cpp
@@ -0,0 +1,17 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Tutorial4Part2/matrix.cpp"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace Lab5UnitTest
+{
+	TEST_CLASS(Lab5UnitTest)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
